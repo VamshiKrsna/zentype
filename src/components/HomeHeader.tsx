01c01a6ad0432d2df83a22c858c283b38459d048
VamshiKrsna <107762231+VamshiKrsna@users.noreply.github.com>
2025-05-01 22:26:45 +0530

    added vite template

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,7 @@
+function HomeHeader(){
+    return(
+        <h1>This is home of the header</h1>       
+    )
+}
+
+export default HomeHeader;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import HomeHeader from "../components/homeHeader";
+
+function Home() {
+  return (
+    <div>
+      <HomeHeader/>
+    </div>
+  );
+}
+
+export default Home;
